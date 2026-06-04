6235f41c20c92fe1e1e2e599182b604dafe88ba5
Shunsuke goto 2121 <wallx717@gmail.com>
2026-06-04 10:14:40 +0900

    update gallery

--- make_gallery.ps1
+++ make_gallery.ps1
@@ -1,4 +1,4 @@
-$imgs = Get-ChildItem -Path "images" -File | Where-Object { $_.Extension -match "\.(jpg|jpeg|png|webp)$" -and $_.Name -ne "gallery.json" } | Select-Object -ExpandProperty Name | Sort-Object
+$imgs = Get-ChildItem -Path "images" -File | Where-Object { $_.Name -match "^gallery-.*\.(jpg|jpeg|png|webp)$" } | Select-Object -ExpandProperty Name | Sort-Object
 $list = ($imgs | ForEach-Object { '"' + $_ + '"' }) -join ","
 $json = '{"images":[' + $list + ']}'
 $json | Set-Content "images/gallery.json" -Encoding UTF8
